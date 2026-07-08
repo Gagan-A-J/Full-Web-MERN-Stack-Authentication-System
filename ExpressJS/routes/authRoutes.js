7e41e9712988a5c4487b5850d7694661222e6457
Gagan A J <gagangaganaj24@gmail.com>
2026-07-08 20:55:50 +0530

    It is defined the user routes for each pages in Backend

---
+++ ExpressJS/routes/authRoutes.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const { signup, login, logout, reqresetpassword, resetpassword, verify } = require('../controllers/authController');
+const router = express.Router();
+const authMiddleware = require('../middleware/authMiddleware');
+
+router.post('/signup', signup);
+router.post('/login', login);
+//router.post('/google-oauth-login', googleOAuthLogin);
+router.post('/logout', logout);
+router.post('/forgot-password', reqresetpassword);
+router.post('/reset-password/:token', resetpassword);
+router.post('/verify-otp', verify)
+
+module.exports = router;
