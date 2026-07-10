402e2e6f5f0b56f7f622dbcb4126464f20e9e3df
Gagan A J <gagangaganaj24@gmail.com>
2026-07-10 21:47:43 +0530

    Add the Password Regex using Alphanum, special, long characters and add the password strength based on password regex and Add the Show or Hide password icon using react icons and import it

--- ReactJS/src/Signup.jsx
+++ ReactJS/src/Signup.jsx
@@ -2,14 +2,36 @@ import React, { useState } from 'react';
 import axios from 'axios';
 import { useNavigate, Link } from 'react-router-dom';
 
+import { FaEye, FaEyeSlash } from "react-icons/fa";
+
 export default function Signup() {
   const [form, setForm] = useState({ username: '', email: '', password: '', phone: '+91 ' });
+  const [showPassword, setShowPassword] = useState(false);
+  const [showConfirmPassword, setShowConfirmPassword] = useState(false);
   const navigate = useNavigate();
+
+  const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,20}$/;
+
+  const getStrength = (password) => {
+    let score = 0;
+
+    if (password.length >= 8 && password.length <= 20) score++;
+    if (/[A-Z]/.test(password)) score++;
+    if (/[a-z]/.test(password)) score++;
+    if (/\d/.test(password)) score++;
+    if (/[@$!%*#?&]/.test(password)) score++;
+
+    return score;
+  };
 
   const handleSubmit = async e => {
     e.preventDefault();
     try {
       const res = await axios.post('/api/auth/signup', form);
+      if (!passwordRegex.test(form.password)) {
+        alert(res.data.message);
+        return;
+      }
       alert(res.data.message);
       navigate('/login');
     } catch (err) {
@@ -24,7 +46,68 @@ export default function Signup() {
       <br />
       <input placeholder="Email" type="email" onChange={e => setForm({ ...form, email: e.target.value })} required />
       <br />
-      <input placeholder="Password" type="password" onChange={e => setForm({ ...form, password: e.target.value })} required />
+      <div className="relative mb-4">
+      <input placeholder="Password" type={showPassword ? "text" : "password"} value={form.password} onChange={e => setForm({ ...form, password: e.target.value })} required/>
+      <span
+        onClick={() => setShowPassword(!showPassword)}
+        className="absolute right-1 top-1/2 -translate-y-1/2 cursor-pointer text-gray-500 hover:text-blue-500">
+        {showPassword ? <FaEyeSlash size={20} /> : <FaEye size={20} />}
+      </span>
+      </div>
+      <div className="mt-2 font-bold">
+
+        Password Strength :
+
+        {getStrength(form.password) <= 2 && (
+          <span className="text-red-500"> Weak</span>
+        )}
+
+        {getStrength(form.password) == 3 && (
+          <span className="text-yellow-500"> Medium</span>
+        )}
+
+        {getStrength(form.password) >= 4 && (
+          <span className="text-green-600"> Strong</span>
+        )}
+
+      </div>
+      <div className="text-left mt-3 text-sm">
+
+        <p className={
+          form.password.length >= 8
+            ? "text-green-600" : "text-red-500"
+        }>
+          ✔ Minimum 8 characters
+        </p>
+
+        <p className={
+          /[A-Z]/.test(form.password)
+            ? "text-green-600" : "text-red-500"
+        }>
+          ✔ One uppercase letter
+        </p>
+
+        <p className={
+          /[a-z]/.test(form.password)
+            ? "text-green-600" : "text-red-500"
+        }>
+          ✔ One lowercase letter
+        </p>
+
+        <p className={
+          /\d/.test(form.password)
+            ? "text-green-600" : "text-red-500"
+        }>
+          ✔ One number
+        </p>
+
+        <p className={
+          /[@$!%*#?&]/.test(form.password)
+            ? "text-green-600" : "text-red-500"
+        }>
+          ✔ One special character
+        </p>
+      </div>
       <br />
       <input placeholder="Phone Number" type="tel" value={form.phone} onChange={(e) => setForm({ ...form, phone: e.target.value })} required />
       <br />
