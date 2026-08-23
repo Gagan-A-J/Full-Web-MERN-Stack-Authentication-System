0e40334cd56de830b62c2f859b5cb30a46990007
Gagan A J <gagangaganaj24@gmail.com>
2026-08-23 12:01:30 +0530

    Add the another extra route for signup or login page using the google account

--- ExpressJS/routes/authRoutes.js
+++ ExpressJS/routes/authRoutes.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const { signup, login, logout, reqresetpassword, resetpassword, verify } = require('../controllers/authController');
+const { googleLogin } = require('../controllers/GoogleAuthController');
 const router = express.Router();
 const authMiddleware = require('../middleware/authMiddleware');
 
 router.post('/signup', signup);
 router.post('/login', login);
-//router.post('/google-oauth-login', googleOAuthLogin);
+router.post('/google-login', googleLogin);
 router.post('/logout', logout);
 router.post('/forgot-password', reqresetpassword);
 router.post('/reset-password/:token', resetpassword);
