5622116cb14c895bee42e03fb7cf2aa1c0616462
Gagan A J <gagangaganaj24@gmail.com>
2026-08-23 12:12:32 +0530

    Add the React.StrictMode HTML element with the GoogleOAuthProvider from the @react-oauth/google

--- ReactJS/src/main.jsx
+++ ReactJS/src/main.jsx
@@ -3,9 +3,16 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './App.css';
 import { BrowserRouter } from 'react-router-dom';
+import { GoogleOAuthProvider } from "@react-oauth/google";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
-    <App />
+  <React.StrictMode>
+    <GoogleOAuthProvider
+    clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}
+    >
+      <App />
+    </GoogleOAuthProvider>
+  </React.StrictMode>
   </BrowserRouter>
 );
